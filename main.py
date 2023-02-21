13eda0fbe9b55ec4d07d344efeee86a3c6b701b1
tivoyudha <tivo.yudhap11@gmail.com>
2023-02-21 17:57:12 +0700

    change with bluepy

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-import time
+# import time
 
-from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneUIDFrame
+# from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneUIDFrame
 
-testingList = []
+# def callback(bt_addr, rssi, packet, additional_info):
+#     print("<%s, %d> %s %s %s" % (bt_addr, rssi, packet, additional_info, rssi * 1.0/packet.tx_power))
+
+# scanner = BeaconScanner(callback,
+# # remove following parameter to read all beacons.
+#     packet_filter=EddystoneUIDFrame,
+#     scan_parameters={"interval_ms": 100, "window_ms": 10}
+# )
+# scanner.start()
+# time.sleep(10)
+# scanner.stop()
+
+from beacon_scanner import BeaconScanner, Beacon
+
+scanner = BeaconScanner()
 
 def callback(bt_addr, rssi, packet, additional_info):
-    testingList.append("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
-    print("<%s, %d> %s %s %s" % (bt_addr, rssi, packet, additional_info, rssi * 1.0/packet.tx_power))
+    beacon = Beacon(bt_addr, rssi, packet, additional_info)
+    print("Beacon: {}".format(beacon))
 
-# scan for all TLM frames of beacons in the namespace "12345678901234678901"
-scanner = BeaconScanner(callback,
-    # remove the following line to see packets from all beacons
-    # device_filter=EddystoneFilter(namespace="12345678901234678901"),
-    packet_filter=EddystoneUIDFrame,
-    scan_parameters={"interval_ms": 100, "window_ms": 10}
-)
-scanner.start()
-# time.sleep(10)
-# print("beacon scanned : " + testingList.count)
-# scanner.stop()
+scanner.start_scan(callback)
