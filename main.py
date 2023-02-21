8618474788a9d7e97d282ea26e9d2a7d0999f03d
tivoyudha <tivo.yudhap11@gmail.com>
2023-02-21 17:58:18 +0700

    add comment

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-# import time
+from bluepy import btle
 
-# from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneUIDFrame
+scanner = btle.Scanner()
 
-# def callback(bt_addr, rssi, packet, additional_info):
-#     print("<%s, %d> %s %s %s" % (bt_addr, rssi, packet, additional_info, rssi * 1.0/packet.tx_power))
+devices = scanner.scan(2)
+for device in devices:
+    for (adtype, desc, value) in device.getScanData():
+        if value.startswith("4c000215"):  # check for iBeacon prefix
+            uuid_hex = value[8:40]
+            uuid_str = uuid_hex[:8] + "-" + uuid_hex[8:12] + "-" + uuid_hex[12:16] + "-" + uuid_hex[16:20] + "-" + uuid_hex[20:]
+            major_hex = value[40:44]
+            minor_hex = value[44:48]
+            major = int(major_hex, 16)
+            minor = int(minor_hex, 16)
+            
+            print("iBeacon found: UUID={}, major={}, minor={}, RSSI={} dB".format(uuid_str, major, minor, device.rssi))
 
-# scanner = BeaconScanner(callback,
-# # remove following parameter to read all beacons.
-#     packet_filter=EddystoneUIDFrame,
-#     scan_parameters={"interval_ms": 100, "window_ms": 10}
-# )
-# scanner.start()
-# time.sleep(10)
-# scanner.stop()
-
-from beacon_scanner import BeaconScanner, Beacon
-
-scanner = BeaconScanner()
-
-def callback(bt_addr, rssi, packet, additional_info):
-    beacon = Beacon(bt_addr, rssi, packet, additional_info)
-    print("Beacon: {}".format(beacon))
-
-scanner.start_scan(callback)
+            # should submit create transaction
